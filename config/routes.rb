defc37d0d2af2f57f1b6e01d089a14d830f65aaf
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2025-03-22 21:11:48 +0100

    Fixed Routing for root to be Landing Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  root to: "pages#home"
+  root to: "pages#landing"
+  get "home", to: "pages#home", as: :home
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
