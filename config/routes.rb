37e64ad4406d7d0bdde7f6bdc589ac93adf69c99
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2025-03-23 13:31:37 +0100

    Added Banner for Home

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
   root to: "pages#landing"
   get "home", to: "pages#home", as: :home
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
