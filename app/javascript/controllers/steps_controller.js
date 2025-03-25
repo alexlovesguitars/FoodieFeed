e81da273cbf55d0a3bf73d4dfc07ce231ee75d3f
Francois Kergaravat <francois.kergaravat@gmail.com>
2025-03-25 23:07:08 +0100

    implemented JS logic for cookmode, step by step

--- app/javascript/controllers/steps_controller.js
+++ app/javascript/controllers/steps_controller.js
@@ -1,32 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="steps"
 export default class extends Controller {
-  static targets = ["step"];
+  static targets = ["step"]
 
   connect() {
-    console.log("Stimulus controller connected!");
-    this.currentStepIndex = 0; // Start at Step 1
-    this.showStep(this.currentStepIndex);
+    this.currentStepIndex = 0
+    this.showStep()
   }
 
-  showStep(index) {
+  showStep() {
     this.stepTargets.forEach((step, i) => {
-      step.classList.toggle("hidden", i !== index);
-    });
+      step.style.display = i === this.currentStepIndex ? "block" : "none"
+    })
   }
 
   nextStep() {
     if (this.currentStepIndex < this.stepTargets.length - 1) {
-      this.currentStepIndex++;
-      this.showStep(this.currentStepIndex);
+      this.currentStepIndex++
+      this.showStep()
     }
   }
 
   previousStep() {
     if (this.currentStepIndex > 0) {
-      this.currentStepIndex--;
-      this.showStep(this.currentStepIndex);
+      this.currentStepIndex--
+      this.showStep()
     }
   }
 }
