43792e09c5fe5a553527dd6d7b2b4e7460eb39a8
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2025-03-28 08:26:31 +0100

    Added edited Cards- still missing avatar

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_27_145638) do
     t.string "ingredients", default: [], array: true
     t.string "method", default: [], array: true
     t.string "recipe_hashtags", default: [], array: true
+    t.string "link"
     t.string "video_link"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
