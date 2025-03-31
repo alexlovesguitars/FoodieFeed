061ea68338275f6af269997eecd980c22162f531
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2025-03-31 11:07:18 +0200

    Fixed avatar not showing when logged in Issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :creator, :bio, :social, :image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :creator, :bio, :social, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :creator, :bio, :social, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :creator, :bio, :social, :avatar])
   end
 
 end
