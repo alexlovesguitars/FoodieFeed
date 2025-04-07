00e73a1eb36a02023a53e34c56c62f557b5034f0
Dominique <dominiquebertisch@gmail.com>
2025-04-07 19:32:12 +0200

    turbo +  like all of it

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,6 @@ class FavoritesController < ApplicationController
   before_action :set_recipe, only: [:create]
   before_action :set_favorite, only: [:destroy]
 
-
   def index
     @favorite_recipes = current_user.favorite_recipes
   end
@@ -14,36 +13,55 @@ class FavoritesController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
 
     # Create a new favorite
-    favorite = current_user.favorites.new(recipe: @recipe)
+    @favorite = current_user.favorites.new(recipe: @recipe)
 
     @favorites = current_user.favorites
-    duplicate = @favorites.find_by(recipe: @recipe)
 
-    if duplicate
-      redirect_back(fallback_location: recipes_path, alert: "Already in your cookbook.")
-    else
-      favorite.save
+    if @favorite.save
       respond_to do |format|
         flash.now[:notice] = "The recipe has been saved to your cookbook."
-        format.turbo_stream
-        format.html {redirect_to @favorite}
+        format.turbo_stream do
+          if request.referer&.include?(favorites_path) # from cookbook
+            turbo_streams = [
+              turbo_stream.append("recipe-cards", partial: "shared/card", locals: { recipe: @recipe }),
+              turbo_stream.update("modal-icon-#{@recipe.id}", partial: "favorites/favorite_delete",
+                                                              locals: { recipe: @recipe })
+            ]
+            if @favorites.count == 1 # Oly replace if this is the first favorite being added
+              turbo_streams << turbo_stream.remove("empty-cookbook")
+            end
+            render turbo_stream: turbo_streams
+            format.html { redirect_to recipes_path, notice: "The recipe has been removed." }
+          end
+        end
+        format.html { redirect_to @favorite }
       end
+    else
+      flash.now[:alert] = "Something went wrong. Please try again."
     end
   end
 
   def destroy
     @favorite = current_user.favorites.find(params[:id])
     @recipe = @favorite.recipe
+    @favorite_recipes = current_user.favorite_recipes
 
     if @favorite.destroy
       flash.now[:notice] = "The recipe has been removed."
       respond_to do |format|
         format.turbo_stream do
           if request.referer&.include?(favorites_path) # from cookbook
-            render turbo_stream: [
+            turbo_streams = [
+              turbo_stream.update("modal-icon-#{@recipe.id}", partial: "favorites/favorite_create",
+                                                              locals: { recipe: @recipe }),
               turbo_stream.remove("recipe_card_#{@recipe.id}"),
               turbo_stream.remove("recipe_card_#{@recipe.id}")
-          ]
+            ]
+            if @favorite_recipes.empty?
+              turbo_streams << turbo_stream.update("cookbook", partial: "favorites/empty_cookbook",
+                                                               locals: { favorite_recipes: @favorite_recipes })
+            end
+            render turbo_stream: turbo_streams
             format.html { redirect_to recipes_path, notice: "The recipe has been removed." }
           end
         end
@@ -70,5 +88,4 @@ class FavoritesController < ApplicationController
   def set_favorite
     @favorite = Favorite.find_by(user: @user, recipe: @recipe)
   end
-
 end
