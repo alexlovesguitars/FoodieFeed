d6fbcd2956a206908305e74175d6628488b0f07e
Dominique <dominiquebertisch@gmail.com>
2025-04-09 10:11:07 +0200

    fixed sign up/edit stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_07_175855) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_03_181155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_07_175855) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "cookbooks", force: :cascade do |t|
+    t.string "user_notes"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_cookbooks_on_recipe_id"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -74,8 +82,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_07_175855) do
     t.string "ingredients", default: [], array: true
     t.string "method", default: [], array: true
     t.string "recipe_hashtags", default: [], array: true
-    t.string "image_link"
     t.string "video_link"
+    t.string "image_link"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
@@ -112,6 +120,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_07_175855) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cookbooks", "recipes"
   add_foreign_key "favorites", "recipes"
   add_foreign_key "favorites", "users"
   add_foreign_key "notes", "recipes"
